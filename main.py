174ac4ba5519bd321ff3566554f9193984f29837
Sergei <sergeisoly@gmail.com>
2023-05-02 15:29:36 +0000

    update app

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def echo(name):
     val = {"new-name": name}
     return jsonify(val)
 
+@app.route('/who-is-the-best-ml-engineer')
+def best():
+    """Return the best ml engineer name"""
+    print("printing some stuff")
+    return '\"Nikita Radchenko is the best ML engineer. No doubts.\" - Sergei Solovev'
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
